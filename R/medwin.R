94ab735df7b34a2ec1903bce35f726da16d8ff00
Liam D. Bailey <liam.bailey@anu.edu.au>
2015-04-19 17:43:24 +0200

    Add documentation for function medwin

--- R/medwin.R
+++ R/medwin.R
@@ -1,4 +1,21 @@
 #' Return the value of the median climate window
+#' 
+#' Returns the median values for window open and window close
+#' from all climate windows that fall within specified model weights.
+#' 
+#' @param Dataset Output from function \code{\link{climatewin}} with information on all fitted climate windows.
+#' @param CW The model weight cut-off that determines which climate windows are used to calculate the median values.
+#' @return Returns a list item containing median values of window open and window close
+#' @author Liam D. Bailey and Martijn van de Pol
+#' @examples
+#' 
+#' Find median window values for climate windows fitted to the Mass dataset
+#' 
+#' data(MassOutput)
+#' 
+#' medwin(Dataset = MassOutput, CW = 0.95)
+#' 
+#' @export
 
 medwin <- function(Dataset, CW = 0.95){
   
