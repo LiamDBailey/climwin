986e66fa7fc90ff0dff4a386e554340d844427a7
Liam D. Bailey <liam.bailey@anu.edu.au>
2015-07-14 23:03:52 +0200

    Added code for dealing with centring data

--- R/plotbetas.R
+++ R/plotbetas.R
@@ -30,128 +30,163 @@
 
 plotbetas <- function(Dataset, plotallenv, plotall = FALSE){
   
-with(Dataset, {
-  if (plotall == FALSE){
-  BETA <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = ModelBeta)) +
-    geom_tile(aes(fill = ModelBeta)) +
-    scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "") +
-    theme_classic() +
-    theme(panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          axis.line = element_line(size = 0.25, colour = "black"),
-          plot.title = element_text(size = 18),
-          legend.position = c(0.75,0.3)) +
-    ggtitle("Beta linear") +
-    ylab("Window open") +
-    xlab("Window close")
-  
-  if(Dataset$Function[1] == "quad"){
-    BETA2 <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = ModelBetaQ)) +
-      geom_tile(aes(fill = ModelBetaQ)) +
-      scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "") +
-      theme_classic() +
-      theme(panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            axis.line = element_line(size = 0.25, colour = "black"),
-            plot.title = element_text(size = 18),
-            legend.position = c(0.75, 0.3)) +
-      ggtitle("Beta quadratic") +
-      ylab("Window open") +
-      xlab("Window close")}
-  
-  if(Dataset$Function[1] == "cub"){
-    BETA2 <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = ModelBetaQ)) +
-      geom_tile(aes(fill = ModelBetaQ)) +
-      scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "") +
-      theme_classic() +
-      theme(panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            axis.line = element_line(size = 0.25, colour = "black"),
-            plot.title = element_text(size = 18),
-            legend.position = c(0.75, 0.3)) +
-      ggtitle("Beta quadratic") +
-      ylab("Window open") +
-      xlab("Window close")
-    
-    BETA3 <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = ModelBetaC)) +
-      geom_tile(aes(fill = ModelBetaC)) +
-      scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "")+
-      theme_classic() +
-      theme(panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            axis.line = element_line(size = 0.25, colour = "black"),
-            plot.title = element_text(size = 18),
-            legend.position = c(0.75, 0.3)) +
-      ggtitle("Beta cubic") +
-      ylab("Window open") +
-      xlab("Window close")
-  }
-  if(Dataset$Function[1] == "quad"){
-    grid.arrange(BETA, BETA2, nrow = 1)
-  } else {
-    if (Dataset$Function[1] == "cub"){
-      grid.arrange(BETA, BETA2, BETA3, nrow = 1)
+  with(Dataset, {
+    if(is.null(Dataset$WithinGrpMean) == FALSE){
+      WGMEAN <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = WithinGrpMean)) +
+        geom_tile(aes(fill = WithinGrpMean)) +
+        scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "") +
+        theme_classic() +
+        theme(panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              axis.line = element_line(size = 0.25, colour = "black"),
+              plot.title = element_text(size = 18),
+              legend.position = c(0.75,0.3)) +
+        ggtitle("Within group mean coefficient") +
+        ylab("Window open") +
+        xlab("Window close")
+      
+      WGDEV <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = WithinGrpDev)) +
+        geom_tile(aes(fill = WithinGrpDev)) +
+        scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "") +
+        theme_classic() +
+        theme(panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              axis.line = element_line(size = 0.25, colour = "black"),
+              plot.title = element_text(size = 18),
+              legend.position = c(0.75,0.3)) +
+        ggtitle("Within group deviation coefficient") +
+        ylab("Window open") +
+        xlab("Window close")
+      
+      if (plotall == FALSE){
+        grid.arrange(WGMEAN, WGDEV, nrow = 1)
+      } else {
+        plotallenv$BETA  <- WGMEAN
+        plotallenv$BETA2 <- WGDEV
+      }
     } else {
-      BETA
+      if (plotall == FALSE){
+        BETA <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = ModelBeta)) +
+          geom_tile(aes(fill = ModelBeta)) +
+          scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "") +
+          theme_classic() +
+          theme(panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                axis.line = element_line(size = 0.25, colour = "black"),
+                plot.title = element_text(size = 18),
+                legend.position = c(0.75,0.3)) +
+          ggtitle("Beta linear") +
+          ylab("Window open") +
+          xlab("Window close")
+        
+        if(Dataset$Function[1] == "quad"){
+          BETA2 <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = ModelBetaQ)) +
+            geom_tile(aes(fill = ModelBetaQ)) +
+            scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "") +
+            theme_classic() +
+            theme(panel.grid.major = element_blank(),
+                  panel.grid.minor = element_blank(),
+                  axis.line = element_line(size = 0.25, colour = "black"),
+                  plot.title = element_text(size = 18),
+                  legend.position = c(0.75, 0.3)) +
+            ggtitle("Beta quadratic") +
+            ylab("Window open") +
+            xlab("Window close")}
+        
+        if(Dataset$Function[1] == "cub"){
+          BETA2 <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = ModelBetaQ)) +
+            geom_tile(aes(fill = ModelBetaQ)) +
+            scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "") +
+            theme_classic() +
+            theme(panel.grid.major = element_blank(),
+                  panel.grid.minor = element_blank(),
+                  axis.line = element_line(size = 0.25, colour = "black"),
+                  plot.title = element_text(size = 18),
+                  legend.position = c(0.75, 0.3)) +
+            ggtitle("Beta quadratic") +
+            ylab("Window open") +
+            xlab("Window close")
+          
+          BETA3 <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = ModelBetaC)) +
+            geom_tile(aes(fill = ModelBetaC)) +
+            scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "")+
+            theme_classic() +
+            theme(panel.grid.major = element_blank(),
+                  panel.grid.minor = element_blank(),
+                  axis.line = element_line(size = 0.25, colour = "black"),
+                  plot.title = element_text(size = 18),
+                  legend.position = c(0.75, 0.3)) +
+            ggtitle("Beta cubic") +
+            ylab("Window open") +
+            xlab("Window close")
+        }
+        if(Dataset$Function[1] == "quad"){
+          grid.arrange(BETA, BETA2, nrow = 1)
+        } else {
+          if (Dataset$Function[1] == "cub"){
+            grid.arrange(BETA, BETA2, BETA3, nrow = 1)
+          } else {
+            BETA
+          }
+        }
+      } else if (plotall == TRUE){
+        plotallenv$BETA <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = ModelBeta)) +
+          geom_tile(aes(fill = ModelBeta)) +
+          scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "") +
+          theme_classic() +
+          theme(panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                axis.line = element_line(size = 0.25, colour = "black"),
+                plot.title = element_text(size = 18),
+                legend.position = c(0.75, 0.3)) +
+          ggtitle("Beta linear") +
+          ylab("Window open") +
+          xlab("Window close")
+        
+        if (Dataset$Function[1] == "quad"){
+          plotallenv$BETA2 <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = ModelBetaQ)) +
+            geom_tile(aes(fill = ModelBetaQ)) +
+            scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "") +
+            theme_classic() +
+            theme(panel.grid.major = element_blank(),
+                  panel.grid.minor = element_blank(),
+                  axis.line = element_line(size = 0.25, colour = "black"),
+                  plot.title = element_text(size = 18),
+                  legend.position = c(0.75, 0.3)) +
+            ggtitle("Beta quadratic") +
+            ylab("Window open") +
+            xlab("Window close")
+        }
+        
+        if (Dataset$Function[1] == "cub"){
+          plotallenv$BETA2 <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = ModelBetaQ)) +
+            geom_tile(aes(fill = ModelBetaQ)) +
+            scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "") +
+            theme_classic() +
+            theme(panel.grid.major = element_blank(),
+                  panel.grid.minor = element_blank(),
+                  axis.line = element_line(size = 0.25, colour = "black"),
+                  plot.title = element_text(size = 18),
+                  legend.position = c(0.75, 0.3)) +
+            ggtitle("Beta quadratic") +
+            ylab("Window open") +
+            xlab("Window close")
+          
+          plotallenv$BETA3 <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = ModelBetaC)) +
+            geom_tile(aes(fill = ModelBetaC)) +
+            scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "") +
+            theme_classic() +
+            theme(panel.grid.major = element_blank(),
+                  panel.grid.minor = element_blank(),
+                  axis.line = element_line(size = 0.25, colour = "black"),
+                  plot.title = element_text(size = 18),
+                  legend.position = c(0.75, 0.3)) +
+            ggtitle("Beta cubic") +
+            ylab("Window open") +
+            xlab("Window close")
+        }
+      } 
     }
-  }
-} else if (plotall == TRUE){
-  plotallenv$BETA <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = ModelBeta)) +
-    geom_tile(aes(fill = ModelBeta)) +
-    scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "") +
-    theme_classic() +
-    theme(panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          axis.line = element_line(size = 0.25, colour = "black"),
-          plot.title = element_text(size = 18),
-          legend.position = c(0.75, 0.3)) +
-    ggtitle("Beta linear") +
-    ylab("Window open") +
-    xlab("Window close")
-  
-  if (Dataset$Function[1] == "quad"){
-    plotallenv$BETA2 <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = ModelBetaQ)) +
-      geom_tile(aes(fill = ModelBetaQ)) +
-      scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "") +
-      theme_classic() +
-      theme(panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            axis.line = element_line(size = 0.25, colour = "black"),
-            plot.title = element_text(size = 18),
-            legend.position = c(0.75, 0.3)) +
-      ggtitle("Beta quadratic") +
-      ylab("Window open") +
-      xlab("Window close")
-  }
-  
-  if (Dataset$Function[1] == "cub"){
-    plotallenv$BETA2 <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = ModelBetaQ)) +
-      geom_tile(aes(fill = ModelBetaQ)) +
-      scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "") +
-      theme_classic() +
-      theme(panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            axis.line = element_line(size = 0.25, colour = "black"),
-            plot.title = element_text(size = 18),
-            legend.position = c(0.75, 0.3)) +
-      ggtitle("Beta quadratic") +
-      ylab("Window open") +
-      xlab("Window close")
-    
-    plotallenv$BETA3 <- ggplot(Dataset, aes(x = WindowClose, y = WindowOpen, z = ModelBetaC)) +
-      geom_tile(aes(fill = ModelBetaC)) +
-      scale_fill_gradientn(colours = c("red", "yellow", "blue"), name = "") +
-      theme_classic() +
-      theme(panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            axis.line = element_line(size = 0.25, colour = "black"),
-            plot.title = element_text(size = 18),
-            legend.position = c(0.75, 0.3)) +
-      ggtitle("Beta cubic") +
-      ylab("Window open") +
-      xlab("Window close")
-  }
-}
 }
 )
 }
