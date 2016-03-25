a28a5daece0e3068c1761a7384576b9d8a9f3c2d
Liam D. Bailey <liam.bailey@anu.edu.au>
2016-03-25 12:32:27 +1100

    Add circle function

--- R/otherfunctions.R
+++ R/otherfunctions.R
@@ -1034,3 +1034,13 @@ merge_results <- function(dataset1, dataset2){
   return(new_dataset)
   
 }
+
+##################################################################################
+
+circle <- function(centre = c(0,0), diameter = 1, npoints = 100){
+  r = diameter / 2
+  tt <- seq(0,2*pi,length.out = npoints)
+  xx <- center[1] + r * cos(tt)
+  yy <- center[2] + r * sin(tt)
+  return(data.frame(x = xx, y = yy))
+}
