dbfba0fe56c67b6c1200e97ff111351a6c0dad0e
Liam D. Bailey <liam.bailey@anu.edu.au>
2016-12-08 14:46:25 +1100

    Include test to ensure that fast method code is not interfering with analyses.

--- tests/testthat/testrandwin.R
+++ tests/testthat/testrandwin.R
@@ -37,6 +37,45 @@ test_that("Check randwin output with slidingwin", {
 
 ############################################################
 
+# Test randwin function with mean climate#
+test_that("Check randwin output with slidingwin", {
+  
+  data(Mass, envir = environment())
+  data(MassClimate, envir = environment())
+  
+  furthest = 2
+  closest = 1
+  stat = "max"
+  repeats = 2
+  
+  rand <- randwin(repeats = 2, xvar = list(Temp = MassClimate$Temp), cdate = MassClimate$Date, 
+                  bdate = Mass$Date, baseline = lm(Mass ~ 1, data = Mass), 
+                  range = c(2, 1), 
+                  type = "relative", stat = "mean", func = "lin", cmissing = FALSE,
+                  window = "sliding")
+  
+  duration  <- (furthest - closest) + 1
+  maxmodno  <- (duration * (duration + 1)) / 2
+  
+  # Test that randwin produces an output
+  expect_true(is.list(rand))
+  
+  # Test that there are no NA values in output
+  expect_equal(length(which(is.na(rand[[1]][, 4]))), 0)
+  
+  # Test that the randomised output has the right number of columns
+  expect_true(ncol(rand[[1]]) == 19)
+  
+  # Test that the right number of models has been fitted
+  expect_equal(repeats, nrow(rand[[1]]))
+  
+  # Test that data has been stored as randomised
+  expect_true((rand[[1]]["Randomised"])[1,] == "yes")
+  
+})
+
+############################################################
+
 test_that("Check randwin output works with spatial replication", {
   
   data(Mass, envir = environment())
