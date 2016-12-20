f8f182ceae4efdbb4456256eef8e5f342e81ba00
unknown <u5307828@RSB000792.UDS.anu.edu.au>
2016-12-20 11:57:21 +1100

    Change startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 .onAttach <- function(...) {
   if (!interactive() || stats::runif(1) > 0.5) return()
   
-  intro <- c("For an introduction to climwin see our new vignette. \n",
-             "climwin v1.0.0 has changed some parameter names and levels. See help documentation and release notes for more details."
+  intro <- c("For an introduction to climwin see our vignette. \n",
+             "climwin v1.1.0 has added new features. See help documentation and release notes for more details."
   )
   packageStartupMessage(intro)
 }
