2dc96b45854d15d5f2b3c4f023f871b704311b39
Bailey <LiamB@nioo.knaw.nl>
2017-09-28 12:09:01 +0200

    Update introduction message.

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 .onAttach <- function(...) {
   if (!interactive() || stats::runif(1) > 0.5) return()
   
-  intro <- c("For an introduction to climwin see our vignette. \n",
-             "climwin v1.1.0 has added new features. See help documentation and release notes for more details."
+  intro <- c("To learn how to use climwin see our vignette. \n",
+             "climwin v1.2.0 has added new features. See help documentation and release notes for more details."
   )
   packageStartupMessage(intro)
 }
