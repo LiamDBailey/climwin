ad8937faa8f95657f833a9db7830099b1b23912f
Unknown <liam.bailey@liamdbailey.com>
2019-06-05 13:57:43 +0200

    Add specific tests for absolute at all cintervals.
    Bugs noticed when cinterval is "week" and type = "absolute". There were no specific tests to deal with this, these have now been added to hopefully avoid such bugs in the future.

--- tests/testthat/testdateconverter.R
+++ tests/testthat/testdateconverter.R
@@ -3,7 +3,7 @@
 #############################################
 
 # Test convertdate where cinterval = "day"
-test_that("convertdate works for days", {
+test_that("convertdate works for days and relative", {
 
 data(Mass, envir = environment())
 data(MassClimate, envir = environment())
@@ -42,8 +42,50 @@ expect_true(max(test$cintno) == 17532)
 
 })
 
+test_that("convertdate works for days and absolute", {
+  
+  data(Mass, envir = environment())
+  data(MassClimate, envir = environment())
+  
+  MassClimatedup         <- MassClimate
+  MassClimatedup[17533,] <- MassClimatedup[17532,]
+  
+  test <- convertdate(bdate = Mass$Date, cdate = MassClimate$Date, xvar = MassClimate$Temp,
+                      cohort = lubridate::year(as.Date(Mass$Date, format = "%d/%m/%Y")), refday = c(1, 6),
+                      cinterval = "day", type = "absolute", spatial = NULL, upper = NA, lower = NA, stat = "mean")
+  
+  # Test that no NA dates were produced
+  expect_equal(length(which(is.na(test))), 0)
+  
+  # Test that the first climate date value is 1
+  expect_equal(test$cintno[1], 1)
+  
+  # Test that the number of climate date values produced is equal to the original dataset
+  expect_equal(length(test$cintno), length(test$xvar))
+  
+  # Test that the largest biological date is encompassed in the climate data
+  expect_true((max(test$bintno) %in% test$cintno))
+  
+  # Test for an error when cintreval is incorrect
+  expect_error(convertdate(bdate = Mass$Date, cdate = MassClimate$Date,
+                           cinterval = "R"))
+  
+  # Test for an error when there is duplicate climate data
+  expect_error(convertdate(bdate = Mass$Date, cdate = MassClimatedup$Date,
+                           cinterval = "day"))
+  
+  #Compare to results from previous R version
+  expect_true(round(mean(test$xvar), 1) == 9.8)
+  expect_true(min(test$bintno) == 517)
+  expect_true(max(test$bintno) == 17319)
+  expect_true(max(test$cintno) == 17532)
+  
+})
+
+#######################################################################################
+
 # Test convertdate with cinterval = week
-test_that("convertdate works for weeks", {
+test_that("convertdate works for week and relative", {
   
 data(Mass, envir = environment())
 data(MassClimate, envir = environment())
@@ -71,8 +113,40 @@ expect_true(max(test$cintno) == 2496)
 
 })
 
+# Test convertdate with cinterval = week
+test_that("convertdate works for week and absolute", {
+  
+  data(Mass, envir = environment())
+  data(MassClimate, envir = environment())
+  
+  test <- convertdate(bdate = Mass$Date, cdate = MassClimate$Date, xvar = MassClimate$Temp,
+                      cohort = lubridate::year(as.Date(Mass$Date, format = "%d/%m/%Y")), refday = c(1, 6),
+                      cinterval = "week", type = "absolute", spatial = NULL, upper = NA, lower = NA, stat = "mean")
+  
+  # Test that no NA dates were produced
+  expect_equal(length(which(is.na(test))), 0)
+  
+  # Test that the first climate date value is 1
+  expect_equal(test$cintno[1], 1)
+  
+  # Test that the length of climate values is equal the original climate dataset
+  expect_equal(length(test$cintno), length(test$xvar))
+  
+  # Test that biological dates are in climate dates
+  expect_true((max(test$bintno) %in% test$cintno))
+  
+  #Compare to results from previous R version
+  expect_true(round(mean(test$xvar), 1) == 9.9)
+  expect_true(min(test$bintno) == 74)
+  expect_true(max(test$bintno) == 2466)
+  expect_true(max(test$cintno) == 2496)
+  
+})
+
+##############################################################################
+
 # Test convertdate for cinterval = "month"
-test_that("convertdate works for months", {
+test_that("convertdate works for month and relative", {
   
 data(Mass, envir = environment())
 data(MassClimate, envir = environment())
@@ -100,10 +174,40 @@ expect_true(max(test$cintno) == 576)
 
 })
 
+# Test convertdate for cinterval = "month"
+test_that("convertdate works for month and absolute", {
+  
+  data(Mass, envir = environment())
+  data(MassClimate, envir = environment())
+  
+  test <- convertdate(bdate = Mass$Date, cdate = MassClimate$Date, xvar = MassClimate$Temp,
+                      cohort = lubridate::year(as.Date(Mass$Date, format = "%d/%m/%Y")), refday = c(1, 6),
+                      cinterval = "month", type = "absolute", spatial = NULL, upper = NA, lower = NA, stat = "mean")
+  
+  # Test that no NA dates were produced
+  expect_equal(length(which(is.na(test))), 0)
+  
+  # Test that the first climate date value is 1
+  expect_equal(test$cintno[1], 1)
+  
+  # Test that the length of climate values is equal the original climate dataset
+  expect_equal(length(test$cintno), length(test$xvar))
+  
+  # Test that biological dates are in climate dates
+  expect_true((max(test$bintno) %in% test$cintno))
+  
+  #Compare to results from previous R version
+  expect_true(round(mean(test$xvar), 1) == 9.8)
+  expect_true(min(test$bintno) == 18)
+  expect_true(max(test$bintno) == 570)
+  expect_true(max(test$cintno) == 576)
+  
+})
+
 ######################################################################
 
 # Test that convertdate works with two climate variables, cinterval = "day"
-test_that("convertdate works for days and two climate variables (i.e. xvar2 != NULL)", {
+test_that("convertdate works (days, xvar2, relative).", {
   
 data(Mass, envir = environment())
 data(MassClimate, envir = environment())
@@ -136,8 +240,44 @@ expect_true(max(test$cintno) == 17532)
 
 })
 
+test_that("convertdate works (days, xvar2, absolute)", {
+  
+  data(Mass, envir = environment())
+  data(MassClimate, envir = environment())
+  
+  test <- convertdate(bdate = Mass$Date, cdate = MassClimate$Date, xvar = MassClimate$Temp,
+                      cohort = lubridate::year(as.Date(Mass$Date, format = "%d/%m/%Y")), refday = c(1, 6),
+                      xvar2 = MassClimate$Rain, cinterval = "day", type = "absolute", cross = TRUE,
+                      spatial = NULL, upper = NA, lower = NA, stat = "mean")
+  
+  # Test that no NA dates were produced
+  expect_equal(length(which(is.na(test))), 0)
+  
+  # Test that the first climate date value is 1
+  expect_equal(test$cintno[1], 1)
+  
+  # Test that the length of climate values is equal the original climate dataset
+  expect_equal(length(test$cintno), length(test$xvar))
+  
+  # Test that the length of climate values is equal the original second climate dataset
+  expect_equal(length(test$cintno), length(test$xvar2))
+  
+  # Test that biological dates are in climate dates
+  expect_true((max(test$bintno) %in% test$cintno))
+  
+  #Compare to results from previous R version
+  expect_true(round(mean(test$xvar), 1) == 9.8)
+  expect_true(round(mean(test$xvar2), 1) == 2.6)
+  expect_true(min(test$bintno) == 517)
+  expect_true(max(test$bintno) == 17319)
+  expect_true(max(test$cintno) == 17532)
+  
+})
+
+#############################################################
+
 # Test that convertdate works with two climate variables, cinterval = "week"
-test_that("convertdate works (week, xvar2)", {
+test_that("convertdate works (week, xvar2, relative)", {
   
 data(Mass, envir = environment())
 data(MassClimate, envir = environment())
@@ -170,8 +310,44 @@ expect_true(max(test$cintno) == 2544)
 
 })
 
+test_that("convertdate works (week, xvar2, relative)", {
+  
+  data(Mass, envir = environment())
+  data(MassClimate, envir = environment())
+  
+  test <- convertdate(bdate = Mass$Date, cdate = MassClimate$Date, xvar = MassClimate$Temp,
+                      cohort = lubridate::year(as.Date(Mass$Date, format = "%d/%m/%Y")), refday = c(1, 6),
+                      xvar2 = MassClimate$Rain, cinterval = "week", type = "absolute", cross = TRUE,
+                      spatial = NULL, upper = NA, lower = NA, stat = "mean")
+  
+  # Test that no NA dates were produced
+  expect_equal(length(which(is.na(test))), 0)
+  
+  # Test that the first climate date value is 1
+  expect_equal(test$cintno[1], 1)
+  
+  # Test that the length of climate values is equal the original climate dataset
+  expect_equal(length(test$cintno), length(test$xvar))
+  
+  # Test that the length of climate values is equal the original second climate dataset
+  expect_equal(length(test$cintno), length(test$xvar2))
+  
+  # Test that biological dates are in climate dates
+  expect_true((max(test$bintno) %in% test$cintno))
+  
+  #Compare to results from previous R version
+  expect_true(round(mean(test$xvar), 1) == 9.8)
+  expect_true(round(mean(test$xvar2), 1) == 2.6)
+  expect_true(min(test$bintno) == 59)
+  expect_true(max(test$bintno) == 2497)
+  expect_true(max(test$cintno) == 2544)
+  
+})
+
+########################################################################
+
 # Test that convertdate works with two climate variables, cinterval = "month"
-test_that("convertdate works (month, variable)", {
+test_that("convertdate works (month, xvar2, relative)", {
   
 data(Mass, envir = environment())
 data(MassClimate, envir = environment())
@@ -202,4 +378,38 @@ expect_true(min(test$bintno) == 17)
 expect_true(max(test$bintno) == 569)
 expect_true(max(test$cintno) == 576)
 
+})
+
+test_that("convertdate works (month, xvar2, absolute)", {
+  
+  data(Mass, envir = environment())
+  data(MassClimate, envir = environment())
+  
+  test <- convertdate(bdate = Mass$Date, cdate = MassClimate$Date, xvar = MassClimate$Temp,
+                      cohort = lubridate::year(as.Date(Mass$Date, format = "%d/%m/%Y")), refday = c(1, 6),
+                      xvar2 = MassClimate$Rain, cinterval = "month", type = "absolute", cross = TRUE,
+                      spatial = NULL, upper = NA, lower = NA, stat = "mean")
+  
+  # Test that no NA dates were produced
+  expect_equal(length(which(is.na(test))), 0)
+  
+  # Test that the first climate date value is 1
+  expect_equal(test$cintno[1], 1)
+  
+  # Test that the length of climate values is equal the original climate dataset
+  expect_equal(length(test$cintno), length(test$xvar))
+  
+  # Test that the length of climate values is equal the original second climate dataset
+  expect_equal(length(test$cintno), length(test$xvar2))
+  
+  # Test that biological dates are in climate dates
+  expect_true((max(test$bintno) %in% test$cintno))
+  
+  #Compare to results from previous R version
+  expect_true(round(mean(test$xvar), 1) == 9.8)
+  expect_true(round(mean(test$xvar2), 1) == 2.6)
+  expect_true(min(test$bintno) == 18)
+  expect_true(max(test$bintno) == 570)
+  expect_true(max(test$cintno) == 576)
+  
 })
