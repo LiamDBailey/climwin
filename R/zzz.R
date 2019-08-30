3bb8e76ee175d7dd02c07678120caa13c24fe11b
Unknown <liam.bailey@liamdbailey.com>
2019-08-30 12:45:44 +0200

    Save test recursive function
    A recursive function may be an alternative to using a loop. This would allow us to skip model refitting if the previous model has the same data (e.g. in cases where the summary statistic is max/min). Currently not active, but worth keeping for later use.

--- R/zzz.R
+++ R/zzz.R
@@ -6,3 +6,62 @@
   )
   packageStartupMessage(intro)
 }
+
+
+#############
+
+#Prototype recursive function that could be used to 
+#reduce model fitting by comparing to the old model
+#This is probably only relevant for cases where max/min
+#are used as the summary stats.
+#Ideally, we would run this so that short windows
+#are run in order (because short models are more likely to be similar)
+
+recursive_func <- function(old_model, row){
+  
+  model_data <- iris
+  model_data$Sepal.Length <- model_data$Sepal.Length * round(runif(n = 1, min = 1, max = 3))
+  
+  if(row == 0){
+    
+    if(identical(model_data$Sepal.Length, model.frame(old_model)$Sepal.Length)){
+      
+      model_row <- data.frame(intercept = coef(old_model)[1],
+                              slope = coef(old_model)[2])
+      
+      return(dplyr::bind_rows(model_row))
+      
+    } else {
+      
+      new_model <- eval(getCall(old_model))
+      
+      model_row <- data.frame(intercept = coef(new_model)[1],
+                              slope = coef(new_model)[2])
+      
+      return(dplyr::bind_rows(model_row))
+      
+    }
+    
+  } else {
+    
+    if(identical(model_data$Sepal.Length, model.frame(old_model)$Sepal.Length)){
+      
+      model_row <- data.frame(intercept = coef(old_model)[1],
+                              slope = coef(old_model)[2])
+      
+      return(dplyr::bind_rows(model_row, recursive_func(old_model, row = row - 1)))
+      
+    } else {
+      
+      new_model <- eval(getCall(old_model))
+      
+      model_row <- data.frame(intercept = coef(new_model)[1],
+                              slope = coef(new_model)[2])
+      
+      return(dplyr::bind_rows(model_row, recursive_func(new_model, row = row - 1)))
+      
+    }
+    
+  }
+  
+}
