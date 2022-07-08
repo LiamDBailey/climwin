dd2f2145e109db6a784d0ed3dc4dc82abb0e5bf7
Liam Bailey <liam.bailey@liamdbailey.com>
2022-07-08 17:21:16 +0200

    Create extractor to get dataset from climwin obj

---
+++ R/extractors.R
@@ -0,0 +1,22 @@
+get_dataset <- function(object, n){
+  
+  if (missing(object) | !inherits(object, "climwin")) {
+    
+    stop("Please provide a climwin results object")
+    
+  }
+  
+  #There will always be 2 objects because of combos
+  if (length(object) == 2) {
+    
+    n <- 1
+    
+  } else if (missing(n)){
+    
+    stop("Please specify which set of climwin results to extract using argument `n`")
+    
+  }
+  
+  return(object[[n]]$Dataset)
+  
+}
