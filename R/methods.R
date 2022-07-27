7c74b13a236cbc99c030368217c1a02893089814
Liam Bailey <liam.bailey@liamdbailey.com>
2022-07-27 15:08:26 +0200

    :sparkles: Summary method for climwinfit

--- R/methods.R
+++ R/methods.R
@@ -11,13 +11,42 @@ print.climwin <- function(x){
   }
 }
 
+summary.climwinfit <- function(x){
+  
+  data <- x$Dataset[1, ]
+  
+  if (data$Type == "absolute") {
+    
+    WindowOpen_date  <- as.Date(paste(year(Sys.Date()), data$Reference.month, data$Reference.day, sep = "-")) - data$WindowOpen
+    WindowClose_date <- as.Date(paste(year(Sys.Date()), data$Reference.month, data$Reference.day, sep = "-")) - data$WindowClose
+    
+    WindowOpen_date  <- format.Date(WindowOpen_date, "%B-%d")
+    WindowClose_date <- format.Date(WindowClose_date, "%B-%d")
+    
+  } else {
+    
+    WindowOpen_date  <- NA
+    WindowClose_date <- NA
+    
+  }
+  
+  cat("\nBest window:", "--------------------", sep = "\n")
+  cat(paste0("Window open: ", data$WindowOpen, " (Date: ", WindowOpen_date, ")\n"))
+  cat(paste0("Window close: ", data$WindowClose, " (Date: ", WindowClose_date, ")\n"))
+  cat(paste0("\U0394", "AICc: ", round(data$deltaAICc, 2), "\n"))
+  
+  cat("\nTop model:\n", "--------------------", sep = "")
+  print(x$BestModel)
+  
+}
+
 summary.climwin <- function(x){
   
   for(i in 1:nrow(x$combos)) {
     
     cat("\n--------------------", paste0("Model #", i), "--------------------", sep = "\n")
     print(x$combos[i, ])
-    print(summary(x[[i]]$BestModel))
+    summary.climwinfit(x[[1]])
     
   }
   
